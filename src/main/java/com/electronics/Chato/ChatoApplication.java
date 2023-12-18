6d7f188d50b7297fbcf7fca13df06f95a40457c5
m-jones001 <147059091+m-jones001@users.noreply.github.com>
2023-12-18 17:23:45 +0300

    Update ChatoApplication.java

--- src/main/java/com/electronics/Chato/ChatoApplication.java
+++ src/main/java/com/electronics/Chato/ChatoApplication.java
@@ -3,7 +3,8 @@ package com.electronics.Chato;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//to enable auto configuration,  component scan and configuration
+@SpringBootApplication 
 public class ChatoApplication {
 
 	public static void main(String[] args) {
